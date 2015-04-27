8f5b93ac01762b59e98498fed7d11146264eb360
gkucia <giorgi.kucia01@geolab.edu.ge>
2015-04-27 21:32:49 +0400

    დავალება

--- Lesson4/Program.cs
+++ Lesson4/Program.cs
@@ -6,10 +6,69 @@ using System.Threading.Tasks;
 
 namespace Lesson4
 {
+    class Lab {
+
+        private int rnd1;
+        private int rnd2;
+
+        public int random1 {
+            get 
+            {
+                return this.rnd1;
+            }
+            set {
+                if (1 <= value && value <= 20)
+                {
+                    this.rnd1 = value;
+                }
+                else 
+                {
+                    Console.WriteLine("Enter number from 1 to 20");
+                }
+            }
+
+        }
+
+   
+        public int random2 {
+            get {
+                return this.rnd2;
+            }
+            set {
+                this.rnd2 = value;
+            }
+        }
+
+        public void print(int a, int b) {
+            rnd1 = a;
+            rnd2 = b;
+            Console.WriteLine(rnd1);
+            Console.WriteLine(rnd2);
+            
+        }
+
+    
+    }
+
+
+
     class Program
     {
         static void Main(string[] args)
         {
+
+            Random rnd = new Random();
+
+            Lab l = new Lab();
+            //l.random1 = Convert.ToInt32(Console.ReadLine());
+            //l.random2 = rnd.Next(0, 30);
+
+            int i = Convert.ToInt32(Console.ReadLine());
+            int k = rnd.Next(0, 30);
+
+            l.print(i, k);
+
+            
         }
     }
 }
